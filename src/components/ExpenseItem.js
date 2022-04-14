18b3d4d4c0db3c9f8098841e1bc1fea9fd483d29
Ray Decker Jr <73715881+RayDeckerJr@users.noreply.github.com>
2022-04-14 05:02:04 -0400

    v.0.1

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,20 @@
 import './ExpenseItem.css';
+import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
 
-    const expenseDate = new Date(2021,2,28);
-    const expenseTitle = "Title";
-    const expenseAmount = "amount";
 
     return (
-        <div className="expense-item">
-            <div>{expenseDate.toISOString()}</div>
+        <Card className ="expense-item">
+
+            <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
-                <h2>{expenseTitle}</h2>
-                <div className="expense-item__price">{expenseAmount}</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
             </div>
-        </div>
+
+        </Card>
     );
 }
 
